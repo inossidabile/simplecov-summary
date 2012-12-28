f8f57027b86dd25ae6945c8bb27d2a4d5fde9962
Boris Staal <boris@roundlake.ru>
2012-12-29 04:58:18 +0700

    Correct source for groups percentage

--- lib/simplecov-summary.rb
+++ lib/simplecov-summary.rb
@@ -9,8 +9,7 @@ class SimpleCov::Formatter::SummaryFormatter
     name_length = (result.groups.keys + ["Total"]).map{|x| x.length}.max
 
     result.groups.each do |name, files|
-      percentage = files.map{|x| x.covered_percent.round(2)}
-      percentage = (percentage.sum / percentage.count).round(2)
+      percentage = files.covered_percent.round(2)
 
       color = case percentage
       when 90..100
